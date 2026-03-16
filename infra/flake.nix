2bf97bf36e3b031c740473cda06c29f99ccb3001
Carlos Anselmo Mendes Junior <cjuniorfox@gmail.com>
2026-03-16 16:24:52 -0300

    feat: rename k8s master configuration to k8s control in flake.nix

--- infra/flake.nix
+++ infra/flake.nix
@@ -7,10 +7,10 @@
 
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
-      k8s-master-1 = nixpkgs.lib.nixosSystem {
+      k8s-control = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./hosts/k8s-master-1/configuration.nix
+          ./hosts/k8s-control/configuration.nix
         ];
       };
     };
