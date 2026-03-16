51e7bc79d26bf060210d538df269e4de568b9dc8
Carlos Anselmo Mendes Junior <cjuniorfox@gmail.com>
2026-03-16 16:23:00 -0300

    feat: configure cluster CIDR and service cluster IP range for Kubernetes

--- infra/modules/kubernetes/master.nix
+++ infra/modules/kubernetes/master.nix
@@ -4,6 +4,11 @@
     roles = [ "master" ];
     masterAddress = "k8s-control.vms.lan";
     easyCerts = true;
+
+    clusterCidr = "10.244.0.0/16,fd00:10:244::/56";
+    serviceClusterIpRange = "10.96.0.0/12,fd00:10:96::/108";
+
+    addonManager.enable = true;
   };
   networking.firewall.allowedTCPPorts = [
     22
