ad39caaef21b2cf27c3065799319502e9234fb2b
Carlos Junior <cjuniorfox@gmail.com>
2026-03-22 22:00:14 -0300

    fix: add IPv4 gateway configuration for ens19 interface in k8s-control

--- infra/hosts/k8s-control/configuration.nix
+++ infra/hosts/k8s-control/configuration.nix
@@ -15,4 +15,5 @@
 
   networking.hostName = "k8s-control";
   networking.interfaces.ens19.ipv4.addresses = [ { address = "172.16.88.11"; prefixLength = 24; } ];
+  networking.interfaces.ens19.ipv4.gateway = "172.16.88.1";
 }
