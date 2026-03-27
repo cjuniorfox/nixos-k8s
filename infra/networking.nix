a7183c491bd7c0a48328c451092a42dea1d59dae
Carlos Junior <cjuniorfox@gmail.com>
2026-03-27 10:40:44 -0300

    fix: remove net.ipv4.ip_forward setting from networking configuration

--- infra/networking.nix
+++ infra/networking.nix
@@ -3,7 +3,6 @@
   networking.firewall.enable = false;
 
   boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
     "net.ipv4.conf.all.rp_filter" = 0;
   };
